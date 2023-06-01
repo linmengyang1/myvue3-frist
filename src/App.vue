fb35ec7ef7c17bb0b22ec08f7f84bd7d881c21b1
linmengyang <linmengyang@itcast.cn>
2023-06-01 22:29:42 +0800

    axios基础配置

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,5 @@
 </template>
 
 <style lang="less" scoped> 
-
+ 
 </style>
---
+++ src/apis/textAPI.js
@@ -0,0 +1,9 @@
+  //## 3. 封装请求函数并测试
+
+  import httpInstance from '@/utils/http'
+  
+  export function getCategory () {
+    return httpInstance({
+      url: 'home/category/head'
+    })
+} 
