4469071f906187e471cff7bdd984860efea2493b
linmengyang <linmengyang@itcast.cn>
2023-06-11 11:20:17 +0800

    第四天搞定

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import '@/styles/common.scss'
 
 //引入懒加载插件指令 并注册
 import {lazyPlugin} from '@/directives'
+//引入全局组件插件
+import {componentPlugin} from '@/components/index.js'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
 
