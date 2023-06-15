7a79473a6ae1c264477b2d9d494151223ea6e8e0
linmengyang <linmengyang@itcast.cn>
2023-06-15 11:38:06 +0800

    结算支付

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
+import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // path和component对应关系的位置
@@ -33,9 +36,21 @@ const router = createRouter({
         {
           
     
-      path: 'cartlist',
-      component: CartList
+          path: 'cartlist',
+          component: CartList
     
+        },
+        {
+          path: 'checkout',
+          component: Checkout
+        },
+        {
+          path: 'pay',
+          component: Pay
+        },
+        {
+          path: 'paycallback',
+          component: PayBack
         }
       ]
     },
